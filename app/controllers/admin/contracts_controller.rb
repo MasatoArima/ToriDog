980cf550f1aa953154cf169e305a91bb12603e84
MasatoArima <afoc.0707@gmail.com>
2021-11-09 04:40:55 +0000

    [Add]admin側ページ作成

--- app/controllers/admin/contracts_controller.rb
+++ app/controllers/admin/contracts_controller.rb
@@ -1,2 +1,23 @@
 class Admin::ContractsController < ApplicationController
+  before_action :setup
+
+  def index
+  end
+  def show
+    @contract = Contract.find(params[:id])
+    @application = Application.find_by(id: @contract.application_id )
+    @dog = Dog.find_by(id: @application.request.dog_id )
+    @dog_owner = @customers.find(@requests.find(@applications.find(@contract.application_id).request_id).customer_id)
+    @trimmer = @customers.find(@applications.find(@contract.application_id).customer_id)
+  end
+
+  private
+
+  def setup
+    @contracts = Contract.all
+    @applications = Application.all
+    @requests = Request.all
+    @customers = Customer.all
+  end
+
 end
