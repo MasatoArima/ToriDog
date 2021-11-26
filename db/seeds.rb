c262138c86a90f6448d88b0a54c97ea817ebdd13
MasatoArima <afoc.0707@gmail.com>
2021-11-26 06:45:05 +0000

    [Fix]seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,78 +13,78 @@
   )
 
   # 会員
-  1.times do |n|
-      Customer.create!(
-        email: "hanako#{n + 1}@test.com",
-        password: "111111",
-        last_name: "山田",
-        first_name: "花子#{n + 1}",
-        last_name_kana: "ヤマダ",
-        first_name_kana: "ハナコ",
-        prefecture_code: "東京都",
-        city: "江東区",
-        street: "test2",
-        post_code: "0000000",
-        phone_number: "09012345678",
-        user_status: 0,
-      )
-  end
-  1.times do |n|
-      Customer.create!(
-        email: "hana#{n + 1}@test.com",
-        password: "111111",
-        last_name: "山田",
-        first_name: "花#{n + 1}",
-        last_name_kana: "ヤマダ",
-        first_name_kana: "ハナコ",
-        prefecture_code: "埼玉県",
-        city: "川越市",
-        street: "test2",
-        post_code: "0000000",
-        phone_number: "09012345678",
-        user_status: 0,
-      )
-  end
-  7.times do |n|
-      Customer.create!(
-        email: "tatuo#{n + 1}@test.com",
-        password: "111111",
-        last_name: "山田",
-        first_name: "達夫#{n + 1}",
-        last_name_kana: "ヤマダ",
-        first_name_kana: "ハナコ",
-        prefecture_code: "東京都",
-        city: "港区",
-        street: "test2",
-        post_code: "0000000",
-        phone_number: "09012345678",
-        user_status: 1,
-      )
-  end
-  7.times do |n|
-      Customer.create!(
-        email: "tat#{n + 1}@test.com",
-        password: "111111",
-        last_name: "山田",
-        first_name: "達夫#{n + 1}",
-        last_name_kana: "ヤマダ",
-        first_name_kana: "ハナコ",
-        prefecture_code: "埼玉県",
-        city: "所沢市",
-        street: "test2",
-        post_code: "0000000",
-        phone_number: "09012345678",
-        user_status: 1,
-      )
-  end
+  # 1.times do |n|
+  #     Customer.create!(
+  #       email: "hanako#{n + 1}@test.com",
+  #       password: "111111",
+  #       last_name: "山田",
+  #       first_name: "花子#{n + 1}",
+  #       last_name_kana: "ヤマダ",
+  #       first_name_kana: "ハナコ",
+  #       prefecture_code: "東京都",
+  #       city: "江東区",
+  #       street: "test2",
+  #       post_code: "0000000",
+  #       phone_number: "09012345678",
+  #       user_status: 0,
+  #     )
+  # end
+  # 1.times do |n|
+  #     Customer.create!(
+  #       email: "hana#{n + 1}@test.com",
+  #       password: "111111",
+  #       last_name: "山田",
+  #       first_name: "花#{n + 1}",
+  #       last_name_kana: "ヤマダ",
+  #       first_name_kana: "ハナコ",
+  #       prefecture_code: "埼玉県",
+  #       city: "川越市",
+  #       street: "test2",
+  #       post_code: "0000000",
+  #       phone_number: "09012345678",
+  #       user_status: 0,
+  #     )
+  # end
+  # 7.times do |n|
+  #     Customer.create!(
+  #       email: "tatuo#{n + 1}@test.com",
+  #       password: "111111",
+  #       last_name: "山田",
+  #       first_name: "達夫#{n + 1}",
+  #       last_name_kana: "ヤマダ",
+  #       first_name_kana: "ハナコ",
+  #       prefecture_code: "東京都",
+  #       city: "港区",
+  #       street: "test2",
+  #       post_code: "0000000",
+  #       phone_number: "09012345678",
+  #       user_status: 1,
+  #     )
+  # end
+  # 7.times do |n|
+  #     Customer.create!(
+  #       email: "tat#{n + 1}@test.com",
+  #       password: "111111",
+  #       last_name: "山田",
+  #       first_name: "達夫#{n + 1}",
+  #       last_name_kana: "ヤマダ",
+  #       first_name_kana: "ハナコ",
+  #       prefecture_code: "埼玉県",
+  #       city: "所沢市",
+  #       street: "test2",
+  #       post_code: "0000000",
+  #       phone_number: "09012345678",
+  #       user_status: 1,
+  #     )
+  # end
 
-  Dog.create!(
-  customer_id: 1  ,
-  name: 'ぎんた',
-  name_kana: 'ギンタ',
-  breed: 'チワックス',
-  sex: true,
-  size: 1,
-  is_inoculate: false,
-  birthday: 20200806,
-  )
+  # Dog.create!(
+  # customer_id: 1  ,
+  # name: 'ぎんた',
+  # name_kana: 'ギンタ',
+  # breed: 'チワックス',
+  # sex: true,
+  # size: 1,
+  # is_inoculate: false,
+  # birthday: 20200806,
+  # )
